3adedbe293a0235600aae5da72e666882a9a23d6
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-03-31 18:00:16 +0530

    Added:Address Book controller class to demonstrate various HTTP  methods

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -17,26 +17,37 @@ import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
 @RequestMapping("/addressbookservice")
 public class AddressBookController {
 	
+	 //curl localhost:8080/addressbookservice/ -w "\n"
 	@RequestMapping(value = {"","/","/get"})
 	public ResponseEntity<String> getAddressBookData() {
 		return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
 	}
 	
+	 //curl localhost:8080/addressbookservice/get/1 -w "\n"
 	@GetMapping("/get/{perId}")
 	public ResponseEntity<String> getAddressBookData(@PathVariable("perId")int perId) {
 		return new ResponseEntity<String>("Get Call Success for id: "+perId, HttpStatus.OK);
 	}
 	
+	
+	//curl -X POST -H "Content-Type: application/json" -d '{"first_name": "Lisa","last_name": "Roy",
+						//"phone_number": 9858742563,"email":"lisaR21@gmail.com","city":"pune",
+						//"state":"Maharashtra","zip":413665}' "http://localhost:8080/addressbookservice/create" -w "\n"
 	@PostMapping("/create")
 	public ResponseEntity<String> addAddressBookData(@RequestBody AddressBookDTO addressBookDTO) {
 		return new ResponseEntity<String>("Created Address Book Data for: "+addressBookDTO, HttpStatus.OK);
 	}
 	
+	
+	 //curl -X PUT -H "Content-Type: application/json" -d '{"first_name": "Rahul","last_name": "Roy",
+						//"phone_number": 8858742563,"email":"RahulR21@gmail.com","city":"pune",
+						//"state":"Maharashtra","zip":413665}' "http://localhost:8080/addressbookservice/update" -w "\n"
 	@PutMapping("/update")
 	public ResponseEntity<String> updateAddressBookData(@RequestBody AddressBookDTO addressBookDTO) {
 		return new ResponseEntity<String>("Updated Address Book Data for: "+addressBookDTO, HttpStatus.OK);
 	}	
 	
+	 //curl -X DELETE -H "Content-Type: application/json" localhost:8080/addressbookservice/delete/1 -w "\n"
 	@DeleteMapping("/delete/{perId}")
 	public ResponseEntity<String> deleteAddressBookData(@PathVariable("perId") int perId) {
 		return new ResponseEntity<String>("Delete call Success for id: "+perId, HttpStatus.OK);
--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -2,16 +2,29 @@ package com.bridgelabz.addressbookapp.dto;
 
 public class AddressBookDTO {
 
-	public String name;
-	public long phoneNumber;
+	public String first_name;
+	public String last_name;
+	public long phone_number;
+	public String email;
+	public String city;
+	public String state;
+	public long zip;
 	
-	public AddressBookDTO(String name,long phoneNumber) {
-		this.name=name;
-		this.phoneNumber=phoneNumber;
+	public AddressBookDTO(String first_name, String last_name, long phone_number, 
+						String email, String city,String state, long zip) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.phone_number = phone_number;
+		this.email = email;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
 	}
+	
 		
 		@Override
 		public String toString() {
-			return "name=" +name+ ":phoneNumber="+phoneNumber;
+			return "AddressBookDTO [first_name=" + first_name + ", last_name=" + last_name + ", phone_number="
+					+ phone_number + ", email=" + email + ", city=" + city + ", state=" + state + ", zip=" + zip + "]";
 		}
 	}
