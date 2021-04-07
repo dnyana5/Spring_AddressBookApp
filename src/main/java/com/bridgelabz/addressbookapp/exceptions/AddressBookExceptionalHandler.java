36814d847b104c6ede233cea949cb63e609b308c
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-04-07 07:26:05 +0530

    Added: Custom Exception Handler to provide user friendly Error Response in case of validatin fails

---
+++ src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookExceptionalHandler.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.addressbookapp.exceptions;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.bridgelabz.addressbookapp.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice
+
+public class AddressBookExceptionalHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handlerMethodArgumentNotValidException(
+      MethodArgumentNotValidException exceptions){
+      List<ObjectError> errorList = exceptions.getBindingResult().getAllErrors();
+      List<String> errMesg = errorList.stream()
+                              .map(objErr -> objErr.getDefaultMessage())
+                              .collect(Collectors.toList());
+      ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",errMesg);
+      return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+
+    }
+
+}
